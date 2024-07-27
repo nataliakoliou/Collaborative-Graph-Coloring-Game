dcae766dbd0ad785c7242391a7a0131b96a85b78
nataliakoliou <natalykoliou@yahoo.gr>
2024-07-27 17:46:54 +0300

    Define tone feature in colors

--- src/colors.py
+++ src/colors.py
@@ -1,8 +1,9 @@
 from abc import ABC
 
 class Color(ABC):
-    def __init__(self, rgb, encoding):
+    def __init__(self, rgb, tone, encoding):
         self.rgb = rgb
+        self.tone = tone
         self.encoding = encoding
 
     @property
@@ -11,55 +12,55 @@ class Color(ABC):
 
 class Hidden(Color):
     def __init__(self):
-        super().__init__((40, 40, 40), None)
+        super().__init__((40, 40, 40), "neutral", None)
 
 class White(Color):
     def __init__(self):
-        super().__init__((255, 255, 255), None)
+        super().__init__((255, 255, 255), "neutral", None)
 
 class Red(Color):
     def __init__(self):
-        super().__init__((255, 0, 0), None)
+        super().__init__((255, 0, 0), "warm", None)
 
 class Orange(Color):
     def __init__(self):
-        super().__init__((255, 165, 0), None)
+        super().__init__((255, 165, 0), "warm", None)
 
 class Yellow(Color):
     def __init__(self):
-        super().__init__((255, 255, 0), None)
+        super().__init__((255, 255, 0), "warm", None)
 
 class Green(Color):
     def __init__(self):
-        super().__init__((0, 255, 0), None)
+        super().__init__((0, 255, "cool", 0), None)
 
 class Blue(Color):
     def __init__(self):
-        super().__init__((0, 0, 255), None)
+        super().__init__((0, 0, 255), "cool", None)
 
 class Pink(Color):
     def __init__(self):
-        super().__init__((255, 192, 203), None)
+        super().__init__((255, 192, 203), "warm", None)
 
 class Violet(Color):
     def __init__(self):
-        super().__init__((138, 43, 226), None)
+        super().__init__((138, 43, 226), "cool", None)
 
 class Cyan(Color):
     def __init__(self):
-        super().__init__((0, 255, 255), None)
+        super().__init__((0, 255, 255), "cool", None)
 
 class Coral(Color):
     def __init__(self):
-        super().__init__((255, 127, 80), None)
+        super().__init__((255, 127, 80), "warm", None)
 
 class Teal(Color):
     def __init__(self):
-        super().__init__((0, 128, 128), None)
+        super().__init__((0, 128, 128), "cool", None)
 
 class Black(Color):
     def __init__(self):
-        super().__init__((0, 0, 0), None)
+        super().__init__((0, 0, 0), "neutral", None)
 
 COLORS = [Red(), Orange(), Yellow(), Green(), Blue(), Pink(), Violet(), Cyan(), Coral(), Teal()]
 HIDDEN=Hidden()
